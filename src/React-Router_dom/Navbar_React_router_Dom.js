ccecc23970f913fda01e19f658f9c5e52410fb28
SAWANT TUSHAR <sawanttushar796@gmail.com>
2023-12-29 13:13:52 +0530

    navbar style change

--- src/React-Router_dom/Navbar_React_router_Dom.js
+++ src/React-Router_dom/Navbar_React_router_Dom.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 function NavbarReactRouterDom() {
   return (
     <div>
-      <ul style={{display:"flex", justifyContent:"center", gap:"50px"}}>
+      <ul style={{display:"flex", justifyContent:"center", gap:"50px",backgroundColor:"purple"}}>
         <NavLink to={"/"}><li>Home</li></NavLink>
         <NavLink to={"/about"}><li>About</li></NavLink>
         <NavLink to={"/services"}><li>Services</li></NavLink>
